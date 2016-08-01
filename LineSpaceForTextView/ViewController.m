24070cf1caf1617b67b5139213bb70e0a0d618ea
codeliu6572 <codeliu6572@163.com>
2016-08-01 10:29:40 +0800

    LineSpaceForTextView
    给textView设置placeholder，字体颜色和行间距

--- LineSpaceForTextView/ViewController.m
+++ LineSpaceForTextView/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITextViewDelegate>
+{
+    UITextView *_textView;
+}
 @end
 
 @implementation ViewController
@@ -17,6 +19,70 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+
+    
+    _textView = [[UITextView alloc]initWithFrame:CGRectMake(10, 100, 300, 80)];
+    _textView.delegate = self;
+    _textView.text = @"请在这里输入内容";
+    _textView.layer.cornerRadius = 10;
+    _textView.layer.borderWidth = 1;
+    _textView.layer.borderColor = [UIColor blackColor].CGColor;
+    [self.view addSubview:_textView];
+    
+    //又内容时该设置才生效，需要切记
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc]init];
+    paragraphStyle.lineSpacing = 8;
+    NSDictionary *attributes = @{NSFontAttributeName:[UIFont systemFontOfSize:15],NSParagraphStyleAttributeName:paragraphStyle,NSForegroundColorAttributeName:[UIColor grayColor]};
+    _textView.attributedText = [[NSAttributedString alloc]initWithString:_textView.text attributes:attributes];
+
+    
+}
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    //收起键盘
+    [_textView resignFirstResponder];
+    
+}
+
+#pragma mark - UITextViewDelegate
+
+- (BOOL)textViewShouldBeginEditing:(UITextView *)textView
+{
+    //编辑时删除代替placeholder效果的内容
+    if ([_textView.text isEqualToString:@"请在这里输入内容"]) {
+        textView.text = @"";
+        NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc]init];
+        paragraphStyle.lineSpacing = 8;
+        NSDictionary *attributes = @{NSFontAttributeName:[UIFont systemFontOfSize:15],NSParagraphStyleAttributeName:paragraphStyle,NSForegroundColorAttributeName:[UIColor blackColor]};
+        _textView.attributedText = [[NSAttributedString alloc]initWithString:_textView.text attributes:attributes];
+    }
+
+    return YES;
+}
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+    //编辑时把placeholder效果颜色变为正常效果
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc]init];
+    paragraphStyle.lineSpacing = 8;
+    NSDictionary *attributes = @{NSFontAttributeName:[UIFont systemFontOfSize:15],NSParagraphStyleAttributeName:paragraphStyle,NSForegroundColorAttributeName:[UIColor blackColor]};
+    _textView.attributedText = [[NSAttributedString alloc]initWithString:_textView.text attributes:attributes];
+   
+}
+- (void)textViewDidEndEditing:(UITextView *)textView
+{
+    //若编辑结束，内容为空，变最开始placeholder效果
+    if ([textView.text isEqualToString:@""]) {
+        _textView.text = @"请在这里输入内容";
+        NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc]init];
+        paragraphStyle.lineSpacing = 8;
+        NSDictionary *attributes = @{NSFontAttributeName:[UIFont systemFontOfSize:15],NSParagraphStyleAttributeName:paragraphStyle,NSForegroundColorAttributeName:[UIColor grayColor]};
+        _textView.attributedText = [[NSAttributedString alloc]initWithString:_textView.text attributes:attributes];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
